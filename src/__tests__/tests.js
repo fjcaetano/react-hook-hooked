dc6ea7ba765ed8d6f0c404411f9c786963144fab
Flávio Caetano <flavio@vieiracaetano.com>
2019-07-24 20:05:09 -0300

    chore(Global): move source files to src/

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -1,46 +1,46 @@
-const hooked = require('../dist').default;
+const hooked = require("../../dist").default;
 
 const noop = () => {};
-const props = { foo: 'bar', xpto: 123 };
+const props = { foo: "bar", xpto: 123 };
 
-describe('handle props', () => {
-  it('passes received props to the component', () => {
+describe("handle props", () => {
+  it("passes received props to the component", () => {
     const result = hooked(noop)(noop)(props);
     expect(result.props).toEqual(props);
   });
 
   it("overwrites props with hook's return attributes", () => {
-    const hook = () => ({ foo: 'xpto' });
+    const hook = () => ({ foo: "xpto" });
 
-    const result = hooked(hook)(noop)(props)
+    const result = hooked(hook)(noop)(props);
 
     expect(result.props).toEqual({
       xpto: 123,
-      foo: 'xpto',
+      foo: "xpto"
     });
   });
 });
 
-describe('args extractor', () => {
-  it('passthru props when unset', () => {
-    const hook = (receivedProps) => {
+describe("args extractor", () => {
+  it("passthru props when unset", () => {
+    const hook = receivedProps => {
       expect(receivedProps).toEqual(props);
     };
 
     hooked(hook)(noop)(props);
   });
 
-  it('receives given props as arguments', () => {
-    const func = (args) => {
+  it("receives given props as arguments", () => {
+    const func = args => {
       expect(args).toEqual(props);
     };
 
     hooked(noop, func)(noop)(props);
   });
 
-  it('returns values to hook as args', () => {
+  it("returns values to hook as args", () => {
     const func = () => props;
-    const hook = (args) => {
+    const hook = args => {
       expect(args).toEqual(props);
     };
 
@@ -48,21 +48,21 @@ describe('args extractor', () => {
   });
 });
 
-describe('hook', () => {
-  it('receives args from extractor', () => {
+describe("hook", () => {
+  it("receives args from extractor", () => {
     const func = () => props;
-    const hook = (args) => {
+    const hook = args => {
       expect(args).toEqual(props);
     };
 
     hooked(hook, func)(noop)();
   });
 
-  it('return values as component props', () => {
+  it("return values as component props", () => {
     const hook = () => props;
-    
+
     const result = hooked(hook)(noop)();
 
     expect(result.props).toEqual(props);
-  })
+  });
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const passThru = <TArgs extends any>(args: TArgs) => args;
 
 /**
- * 
+ *
  * @param hook The React hook to be added to your component.
  * @param extractArgs An optional function to extract the hook arguments from the component's props.
  */
 const hooked = <Props extends Object, HookRet, HookArgs = Props>(
   hook: (args: HookArgs) => HookRet,
-  extractArgs: (props: Props) => HookArgs = passThru as any,
+  extractArgs: (props: Props) => HookArgs = passThru as any
 ) => (WrappedComponent: React.ComponentType<Props & HookRet>) => (
-  props: Props,
+  props: Props
 ) => <WrappedComponent {...props} {...hook(extractArgs(props))} />;
 
 export default hooked;
-
