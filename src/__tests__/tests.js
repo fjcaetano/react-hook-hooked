8bb0fa641c69ec477519bc75299d415123918e0b
Flávio Caetano <flavio@vieiracaetano.com>
2019-07-29 19:05:50 -0300

    tests(Mocks): use mocks for test functions

--- src/__tests__/tests.js
+++ src/__tests__/tests.js
@@ -10,10 +10,11 @@ describe('handle props', () => {
   });
 
   it("overwrites props with hook's return attributes", () => {
-    const hook = () => ({ foo: 'xpto' });
+    const hook = jest.fn(() => ({ foo: 'xpto' }));
 
     const result = hooked(hook)(noop)(props);
 
+    expect(hook).toHaveBeenCalled();
     expect(result.props).toEqual({
       xpto: 123,
       foo: 'xpto',
@@ -23,46 +24,57 @@ describe('handle props', () => {
 
 describe('args extractor', () => {
   it('passthru props when unset', () => {
-    const hook = receivedProps => {
+    const hook = jest.fn(receivedProps => {
       expect(receivedProps).toEqual(props);
-    };
+    });
 
     hooked(hook)(noop)(props);
+
+    expect(hook).toHaveBeenCalled();
   });
 
   it('receives given props as arguments', () => {
-    const func = args => {
+    const func = jest.fn(args => {
       expect(args).toEqual(props);
-    };
+    });
 
     hooked(noop, func)(noop)(props);
+
+    expect(func).toHaveBeenCalled();
   });
 
   it('returns values to hook as args', () => {
-    const func = () => props;
-    const hook = args => {
+    const func = jest.fn(() => props);
+    const hook = jest.fn(args => {
       expect(args).toEqual(props);
-    };
+    });
 
     hooked(hook, func)(noop)();
+
+    expect(hook).toHaveBeenCalled();
+    expect(func).toHaveBeenCalled();
   });
 });
 
 describe('hook', () => {
   it('receives args from extractor', () => {
-    const func = () => props;
-    const hook = args => {
+    const func = jest.fn(() => props);
+    const hook = jest.fn(args => {
       expect(args).toEqual(props);
-    };
+    });
 
     hooked(hook, func)(noop)();
+
+    expect(hook).toHaveBeenCalled();
+    expect(func).toHaveBeenCalled();
   });
 
   it('return values as component props', () => {
-    const hook = () => props;
+    const hook = jest.fn(() => props);
 
     const result = hooked(hook)(noop)();
 
     expect(result.props).toEqual(props);
+    expect(hook).toHaveBeenCalled();
   });
 });
